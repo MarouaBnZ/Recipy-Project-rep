081d9c5fcfda148093b9545d3f518f3f778da69c
Maroua Bouanz <maroua.bouanz@gmail.com>
2023-09-12 11:33:30 +0100

    Ajouter une barre de menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,36 @@
-import React from "react";
 import styles from "./Header.module.scss";
 import recette from "../assets/images/recetteTr.png";
+import { useState } from "react";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header
       className={`  ${styles.header} d-flex flex-row align-items-center  `}
     >
-      <i class="fa-solid fa-bars mr-15"></i>
+      {/* <i class="fa-solid fa-bars mr-15"></i> */}
       <div className="flex-fill">
         <img src={recette} alt="logo-recette" />
       </div>
-      <ul className="p-5">
+      <ul className={styles.headerList}>
         <button className="mr-5 btn btn-reverse-primary">
-          <i class="fa-solid fa-basket-shopping mr-5"></i>
+          <i class="fa-solid fa-heart mr-5"></i>
           <span>WISHLIST</span>
         </button>
         <button className="btn btn-primary">Connexion</button>
       </ul>
+      <i
+        onClick={() => setShowMenu(true)}
+        className={`fa-solid fa-bars ${styles.headerXs}`}
+      ></i>
+      {showMenu && (
+        <>
+          <div onClick={() => setShowMenu(true)} className="calc"></div>
+          <HeaderMenu />
+        </>
+      )}
     </header>
   );
 }
