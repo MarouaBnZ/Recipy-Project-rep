17dca624810cfa7386bbfc6e92c9cd13ce41e686
Maroua Bouanz <maroua.bouanz@gmail.com>
2023-11-27 11:31:40 +0100

    Avancement_Projet_Recette

--- src/pages/Homepage/HomePage.js
+++ src/pages/Homepage/HomePage.js
@@ -1,22 +1,67 @@
-import React from "react";
 import styles from "./Homepage.module.scss";
 import Recipe from "./components/Recipe/Recipe";
-
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 import Loading from "../../components/Loading/Loading";
+import { ApiContext } from "../../context/ApiContext";
 function HomePage() {
   const [recipes, setRecipes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
   const [filter, setFilter] = useState("");
+  const [page, setPage] = useState(1);
+  const BASE_URL_API = useContext(ApiContext);
+
+  // useEffect
+
+  useEffect(() => {
+    let cancel = false;
+    async function fetchRecipes() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `${BASE_URL_API}?skip=${(page - 1) * 18}&limit=18`
+        );
+        if (response.ok && !cancel) {
+          const newRecipes = await response.json();
+          setRecipes((x) =>
+            Array.isArray(newRecipes)
+              ? [...x, ...newRecipes]
+              : [...x, ...newRecipes]
+          );
+          // Pour changer les recipes en un tableau
+          // SAUVEGARDE DES PHOTOS LOCALES
+          // SKIP ET LIMIT POUR GERER LA PAGINATION
+        }
+      } catch (error) {
+        console.log("ERROR");
+      } finally {
+        if (!cancel) {
+          setIsLoading(false);
+        }
+      }
+    }
+    fetchRecipes();
+    return () => (cancel = true);
+  }, [BASE_URL_API, page]);
+
+  function updateRecipe(updatedRecipe) {
+    setRecipes(
+      recipes.map((r) => (r._id === updatedRecipe._id ? updatedRecipe : r))
+    );
+  }
 
   function handleInput(e) {
     const filter = e.target.value;
     setFilter(filter.trim().toLowerCase());
   }
+
+  function handleClickLoadMoreRecipes() {}
   return (
     <div className=" flex-fill container p-20 d-flex flex-column">
-      <h1 className="my-30"> Decouvrez nos nouvelles recettes </h1>
+      <h1 className="my-30">
+        {" "}
+        Decouvrez nos nouvelles recettes{" "}
+        <small className={styles.small}>{recipes.length}</small>{" "}
+      </h1>
       <div
         className={`card p-20 d-flex flex-column flex-fill mb-20 ${styles.contentCard}`}
       >
@@ -38,10 +83,19 @@ function HomePage() {
             {recipes
               .filter((r) => r.title.toLowerCase().startsWith(filter))
               .map((r) => (
-                <Recipe title={r.title} image={r.image} />
+                <Recipe
+                  key={r.id}
+                  recipe={r}
+                  togglelikedRecipe={updateRecipe}
+                />
               ))}
           </div>
         )}
+        <div className="d-flex flex-row  justify-content-center align-items-center p-20">
+          <button className="btn btn-primary" onClick={() => setPage(page + 1)}>
+            Charger Plus De Recettes
+          </button>
+        </div>
       </div>
     </div>
   );
